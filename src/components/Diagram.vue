27b0f3715de7ad8890c10086ddb26eb65cc69777
KaurTaal <73363524+KaurTaal@users.noreply.github.com>
2021-04-23 00:17:04 +0300

    Diagram (#3)
    * kalkulaatori layout
    
    * kalkulaatori sisestus ja väljastus kastid
    
    * Kaardi ja markeri kuvamine, markeri liigutamine kaardil
    
    * Esialgne diagramm suvaliste andmetega

--- src/components/Diagram.vue
+++ src/components/Diagram.vue
@@ -1,24 +1,57 @@
 <template>
-  <div class="diagram-cont">
-      Olen diagram
-  </div>
+  <v-chart
+      autoresize
+      :option="option"
+  ></v-chart>
 </template>
 
 <script>
+import ECharts from "vue-echarts";
+import "echarts";
+
 export default {
-  name: "diagram"
-}
+  name: "diagram",
+
+  components: {"v-chart": ECharts},
+
+  data() {
+    return {
+      option: {
+        legend: {
+          data: ["Length of day"]
+        },
+        xAxis: {
+          data: ["Date1", "Date2", "Date3", "Date4", "Date5", "Date6"]
+        },
+        yAxis: {},
+        series: [{
+          name: "Length of day",
+          type: "line",
+          data: [1, 2, 3, 4, 6, 7, 8, 9, 10, 11, 12],
+          smooth: true,
+        }],
+        lineStyle:{
+          color: 'black'
+        },
+        stateAnimation:{
+          duration: 300,
+          easing: 'cubicOut'
+        },
+      }
+    }
+  },
+  methods: {
+    dummy() {
+      this.option.series[0].data = [];
+      this.option = JSON.parse(JSON.stringify(this.option))
+    }
+
+  },
+
+};
 </script>
 
 <style scoped>
-.diagram-cont{
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  background-color: lightcoral;
-  width: 100%;
-  height: 100%;
-}
+
 
 </style>
